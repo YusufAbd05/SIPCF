5ed148cbef12ccedfaa5d3b8c8246aeb7ed21d45
YusufAbd05 <abdyusuf05@gmail.com>
2026-04-12 00:48:50 +0700

    model

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,5 +6,15 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
+    protected $table = 't_user';
+    protected $primaryKey = 'id_user';
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = ['nama', 'email', 'no_hp', 'password', 'role'];
 
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 }
